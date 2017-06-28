411ff8cd137e4091386111016d04f4002c2da209
Milan Felix Šulc <rkfelix@gmail.com>
2017-06-28 14:07:31 +0200

    BasicAuth: added afterCompile starting

--- src/DI/BasicAuthExtension.php
+++ src/DI/BasicAuthExtension.php
@@ -3,7 +3,11 @@
 namespace Contributte\Http\DI;
 
 use Contributte\Http\Auth\BasicAuthenticator;
+use LogicException;
 use Nette\DI\CompilerExtension;
+use Nette\Http\IRequest;
+use Nette\Http\IResponse;
+use Nette\PhpGenerator\ClassType;
 use Nette\Utils\Validators;
 
 /**
@@ -35,8 +39,8 @@ class BasicAuthExtension extends CompilerExtension
 		// Skip if its disabled
 		if (!$config['enabled']) return;
 
-		// Skip if there's no user
-		if (!$config['users']) return;
+		// Throws if there's no user
+		if (!$config['users']) throw new LogicException('You have to define any user or disable extension');
 
 		$def = $builder->addDefinition($this->prefix('authenticator'))
 			->setClass(BasicAuthenticator::class, [$config['title']]);
@@ -44,6 +48,27 @@ class BasicAuthExtension extends CompilerExtension
 		foreach ($config['users'] as $user => $password) {
 			$def->addSetup('addUser', [$user, $password]);
 		}
+	}
+
+	/**
+	 * Decorate initialize
+	 *
+	 * @param ClassType $class
+	 * @return void
+	 */
+	public function afterCompile(ClassType $class)
+	{
+		$config = $this->validateConfig($this->defaults);
+
+		// Skip if its disabled or no user defined
+		if (!$config['enabled'] || !$config['users']) return;
+
+		$initialize = $class->methods['initialize'];
+		$initialize->addBody('$this->getService(?)->authenticate($this->getByType(?), $this->getByType(?));', [
+			$this->prefix('authenticator'),
+			IRequest::class,
+			IResponse::class,
+		]);
 	}
 
 }
